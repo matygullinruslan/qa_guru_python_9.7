acfa4e379a5bb6525664fb06b2cbfed6e8c57143
matygullinruslan <matygullinruslan@gmail.com>
2023-12-18 21:56:07 +0300

    file_test

---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+def func_name(func, argument):
+    temp1 = 123
+    temp2 = 456
+    temp4 = 789
+    temp3 = temp1 + temp2 + temp4
+    print(temp3)
+
+
+print('rty')
+func_name('qwerty', 'asd')
--- test_file_zip_9.7.py
+++ test_file_zip_9.7.py
@@ -1,6 +1,36 @@
 import os
+import zipfile
+from typing import TextIO
 from zipfile import ZipFile
+import time
+import shutil
+import content
+
+# z_archive = r"C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\\test_archive.zip"
+# z_archive = ZipFile(z_archive, 'w')  # создаем архив
+# with ZipFile("metanit.zip", "w") as myzip:
+myzip = ZipFile("test_archive.zip", "w")  # создаем архив
+with ZipFile("test_archive.zip", "w") as myzip:  #
+    myzip.write("test_file.txt")
+    myzip.write("test_file.csv")
+    myzip.write("test_file.xlsx")
+    # os.rename('.\\qa_guru_python_9.7\\test_archive.zip', '.\\qa_guru_python_9.7\\tmp\\test_archive.zip')
+os.rename('C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\test_archive.zip',
+          'C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\\test_archive.zip')
+with ZipFile("test_archive.zip", "w") as myzip:
+    content = myzip.read("test_file.txt")
+print(content)
 
 
-with test_archive.zip() as archive:
-    archive.create_entry("file", "file.txt")
+
+
+
+# z_archive.write(r'C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\Test_file.txt')  # добавляем файл 1 в архив
+# z_archive.write(r'C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\\Test_file.xlsx')  # добавляем файл 2 в архив
+# z_archive.write(r'C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\\Test_file.csv')  # добавляем файл в 3 архив
+#
+# zname = ZipFile('C:\\Users\\rusel\\QA.GURU\\qa_guru_python_9.7\\tmp\Test_file.txt\\test_archive.zip')
+# print(zname.namelist())
+
+# myzip = ZipFile("test_archive1.zip", "w")
+# ...
