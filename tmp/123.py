142415ad267da12306c31745b1b1a357cc1fe25e
matygullinruslan <matygullinruslan@gmail.com>
2023-12-19 23:51:18 +0300

    file_test

--- tmp/123.py
+++ tmp/123.py
@@ -1,4 +1,13 @@
 import os
+from zipfile import ZipFile
+import sheet
+from pypdf import PdfReader
+from openpyxl import load_workbook
 
-directory = os.path.join(os.path.dirname(__file__), "resources")
-print(directory)
+# directory = os.path.join(os.path.dirname(__file__), "resources")
+# print(directory)
+
+
+workbook = load_workbook('resources/test_file.xlsx')
+workbook.active
+print(sheet.cell(row=1, column=2).value)
